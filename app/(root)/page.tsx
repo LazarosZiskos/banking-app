3a6bc94f181b9273a72be62b760e9bd2456180b7
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <lazarosziskos@MacBook-Air-LAZAROS.local>
2024-06-16 16:49:07 +0300

    implement chart and total balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,32 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+  const isLoggedIn = {firstName: "Lazaros"}
+
   return (
-    <div>
+    <section className='home'>
+      <div className='home-content'>
+        <header>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={isLoggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently"
+
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks = {1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
       Dashboard
-    </div>
+    </section>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 
 export const metadata: Metadata = {
   title: "Horizon Bank",
-  description: "Horizon is a modern banking platform for everyone",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: "/icons/logo.svg"
   }
