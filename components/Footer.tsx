ee7e426c1a7278fe654b939f493edcc732497347
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2024-07-12 23:02:25 +0300

    first bank connection

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
         className={type === "mobile" ? "footer_email-mobile" : "footer_email"}
       >
         <h1 className="text-14 truncate text-gray-700 font-semibold">
-          {user?.firstName}
+          {user?.name}
         </h1>
         <p className="text-14 truncate font-normal text-gray-600">
           {user?.email}
