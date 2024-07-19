a779cd8b285b0b409663e12bf73ef2e6edbb3f90
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2024-07-19 16:49:55 +0300

    add force-dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
+export const dynamic = "force-dynamic";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const ibmPlexSerif = IBM_Plex_Serif({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif'
-})
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
 
 export const metadata: Metadata = {
   title: "Horizon Bank",
   description: "Horizon is a modern banking platform for everyone.",
   icons: {
-    icon: "/icons/logo.svg"
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
